a7131ce6dcb5cabd8b86a81448f6d70e2f1ef304
Resul Umit <resulumit@gmail.com>
2024-11-19 13:16:52 +0000

    switches to bluesky

--- server.R
+++ server.R
@@ -1,60 +1,107 @@
-# server.R
-
 # load the libraries
 library(shiny)
-library(rtweet)
+library(bskyr)
 library(httpuv)
 library(DT)
 library(dplyr)
 library(stringr)
 source("the_keys.R")
 
-
-# start the function
+# Define the server function
 function(input, output) {
   
-  # start eventReactive
-  clicked <- eventReactive(input$search_now,{
+  # Event reactive function to fetch likes
+  clicked <- eventReactive(input$search_now, {
     
-    # validate for any problems
-    validate(need(length(get_favorites(user = input$liked_by, n = 1)) != 0, 
-                  "Something went wrong. 
-                   - It could be that there is a spelling error in the username.
-                   - It could also be that the servers are overloaded."))
-
-    # get_favorites() from the rtweet package
-    get_favorites(user = input$liked_by)
-      
-  # end eventReactive  
-  }
-  )
-  
-  # start the table
-  output$table <- renderDataTable(
+    # Process the liked_by input to include domain if missing
+    processed_liked_by <- trimws(input$liked_by)
     
-    # the table
-    clicked() %>% 
-      
-      filter(if (!is.null(input$tweeted_by)) str_detect(str_to_lower(screen_name), str_to_lower(input$tweeted_by)),
-             as.Date(created_at) >= input$daterange[1] & as.Date(created_at) <= input$daterange[2],
-             if (!is.null(input$text_search)) str_detect(str_to_lower(text), str_to_lower(input$text_search))) %>% 
-      
-      # 
-      mutate(tw_link = paste0("<a href='","https://twitter.com/", screen_name, "/status/", status_id, "' target='_blank'>", "Click here","</a>"),
-             tw_date = format(as.Date(created_at), "%Y/%m/%d")) %>% 
-      
-      # select variables
-      select("Tweet" = text, "By" = screen_name, "On" = tw_date, "Link" = tw_link),
-                                      
-      # Options: DT
-      options = list(pageLength = 20),
-      
-      # Options: datatable
-      rownames = FALSE,
-      escape = FALSE
+    # If input ends with a dot, append 'bsky.social'
+    if (grepl("\\.$", processed_liked_by)) {
+      processed_liked_by <- paste0(processed_liked_by, "bsky.social")
+    }
+    # If input does not contain any dot, append '.bsky.social'
+    else if (!grepl("\\.", processed_liked_by)) {
+      processed_liked_by <- paste0(processed_liked_by, ".bsky.social")
+    }
+    # Else, keep processed_liked_by as is (it already contains a domain)
     
-    # end the table
+    # Initialize error_message
+    error_message <- NULL
+    
+    # Try to fetch likes using bs_get_likes() with processed_liked_by
+    likes_data <- tryCatch(
+      {
+        bs_get_likes(actor = processed_liked_by)
+      },
+      error = function(e) {
+        # Store the error message
+        error_message <<- e$message
+        # Return NULL to indicate failure
+        return(NULL)
+      }
     )
-
-# end the function
+    
+    # Validate the result and include the processed_liked_by in the message
+    validate(
+      need(!is.null(likes_data), 
+           paste0("\n",
+                  "Something went wrong.\n",
+                  "- It could be that there is a spelling error in the username: ", processed_liked_by, "\n",
+                  "- It could also be that the servers are overloaded.\n",
+                  "\n",
+                  "Original Error message: ", error_message)
+      )
+    )
+    
+    # Return the likes data
+    likes_data
+  })
+  
+  # Render the data table
+  output$table <- renderDataTable({
+    
+    # Process the data
+    clicked() %>% 
+      # Filter based on inputs
+      filter(
+        # Filter by posted_by pattern matching in handle or display name
+        if (input$posted_by != "") {
+          (str_detect(str_to_lower(post_author_handle), str_to_lower(input$posted_by)) | 
+             str_detect(str_to_lower(post_author_display_name), str_to_lower(input$posted_by)))
+        } else TRUE,
+        
+        # Filter by date range
+        as.Date(post_record_created_at) >= input$daterange[1] & as.Date(post_record_created_at) <= input$daterange[2],
+        
+        # Filter by text search pattern
+        if (input$text_search != "") str_detect(str_to_lower(post_record_text), str_to_lower(input$text_search)) else TRUE,
+        
+      ) %>% 
+      # Create link and formatted date
+      mutate(
+        rkey = gsub("^.*app.bsky.feed.post/", "", post_uri),
+        bs_link = paste0(
+          "<a href='https://bsky.app/profile/", 
+          post_author_handle, 
+          "/post/", 
+          rkey, 
+          "' target='_blank'>Click here</a>"
+        ),
+        bs_date = format(as.Date(post_record_created_at), "%Y/%m/%d")
+      ) %>% 
+      # Select columns to display (removed "Handle")
+      select(
+        "Post" = post_record_text, 
+        "By" = post_author_display_name, 
+        "On" = bs_date, 
+        "Link" = bs_link
+      ) %>% 
+      # Return the datatable
+      datatable(
+        options = list(pageLength = 20),
+        rownames = FALSE,
+        escape = FALSE
+      )
+  })
 }
--- ui.R
+++ ui.R
@@ -6,76 +6,150 @@ library(shinyWidgets)
 library(shinyBS)
 library(DT)
 
-# Start: page ----
+# Start the UI
 fluidPage(
   
-# Style the validation message
+  # Style the validation message
   tags$head(tags$style(HTML(".shiny-output-error-validation { color: #ff0000; font-weight: bold; font-size: 15px;}"))),
-
-# App title ----
-  titlePanel("LikeWise"),
-
-# Help text ----
-  helpText("An app that filters users' likes on Twitter", HTML("&mdash;"),
-            "by", a("Resul Umit", href = "https://resulumit.com"), "(2020)."),
-
-
-# Sidebar layout with input and output definitions ----
-  sidebarLayout(
-
-# Sidebar panel for inputs ----
-  sidebarPanel(
-
-helpText("1. Retrieve the tweets"),    
+  
+  tags$head(
     
-# Input: Liked by ----
-textInput(inputId = "liked_by", label = "Liked by", placeholder = "Twitter username"),
-
-# Input: Tooltip for Liked by ----
-bsTooltip(id = "liked_by", title = "Not case or @ sensitive &mdash; anyting goes.",
-          placement = "top", options = list(container = "body")),
-
-# Input: Search ----
-actionButton(inputId = "search_now", label = "Submit"),
-
-HTML("<h2>&nbsp; &nbsp; &nbsp;</h2>"),
-
-helpText("2. Filter the tweets"),
-
-# Input: Tweeted by ----
-textInput(inputId = "tweeted_by", label = "Tweeted by", placeholder = "Twitter username"),
-
-# Input: Tooltip for tweeted by ----
-bsTooltip(id = "tweeted_by", title = "Not case or @ sensitive &mdash; anyting goes.",
-          placement = "top", options = list(container = "body")),
-
-# Input: Date ----
-dateRangeInput("daterange", label = "...within the date range", format = "yyyy/mm/dd", separator = HTML("&ndash;"),
-               start = as.POSIXct(Sys.Date() - 365), end = as.POSIXct(Sys.Date())),
-
-# Input: Scope ----
-textInput(inputId = "text_search", label = "...including the pattern", placeholder = "Filter by text pattern"),
-
-# Input: Tooltip for Scope ----
-bsTooltip(id = "text_search", title = "Searching <i>us</i> catches <i>us</i>, <i>bus</i>, <i>US</i>, <i>USA</i> ...",
-          placement = "top", options = list(container = "body")),
-
-
-# End: sidebarPanel ----
-),
-
-# Start: MainPanel ----
-mainPanel(
-  tabsetPanel(
-    tabPanel("Table", DT::dataTableOutput(outputId = "table")),
-    tabPanel("Notes",
-             br(),
-             p("The source code is available at", a("https://github.com/resulumit/likewise.", href = "https://github.com/resulumit/likewise"),
-             p("Twitter will not return more than 3200 most recent likes. In practice, it may return fewer than this limit at any given request."))
-             )))
-
-# End: sidebarLayout ----
-)
-
-# End: fluidpage ----
+    # CSS Styles for Input Background Colors
+    tags$style(HTML("
+      /* Define colors for specific inputs */
+      .input-background1 {
+          background-color: #d4f0f0;
+          padding: 10px;
+          border-radius: 10px;
+          margin-bottom: 10px;
+      }
+      .input-background2 {
+          background-color: #fff5d6;
+          padding: 10px;
+          border-radius: 5px;
+          margin-bottom: 10px;
+      }
+      .input-background3 {
+          background-color: #EAD4EB;
+          padding: 10px;
+          border-radius: 5px;
+          margin-bottom: 10px;
+      }
+    "),
+               HTML(".shiny-output-error-validation { color: #ff0000; font-weight: bold; font-size: 15px;}")  
+               ),
+    
+    # Meta tag for UTF-8 encoding
+    tags$meta(charset = "UTF-8")
+    
+    # Google Analytics
+    #, includeHTML("google_analytics.html")
+  ),
+  
+  
+  # App title
+  titlePanel("LikeWise"),
+  
+  # Help text
+  helpText(
+    "An app that filters users' likes on Bluesky", HTML("&mdash;"),
+    "by", a("Resul Umit", href = "https://resulumit.com"), "(2024)."
+  ),
+  
+  # Sidebar layout
+  sidebarLayout(
+    
+    # Sidebar panel for inputs
+    sidebarPanel(
+      
+      div(class = "input-background1",
+          
+      helpText("1. Retrieve the posts"),
+      
+      # Input: Liked by
+      textInput(
+        inputId = "liked_by", 
+        label = "Liked by", 
+        placeholder = "Bluesky handle"
+      ),
+      
+      # Tooltip for Liked by
+      bsTooltip(
+        id = "liked_by", 
+        title = "Handles ending with .bsky.social can be entered with or without the domain ending",
+        placement = "top", 
+        options = list(container = "body")
+      ),
+      
+      # Input: Search button
+      actionButton(inputId = "search_now", label = "Submit")
+      
+      ),
+      
+      div(class = "input-background3",
+          
+      helpText("2. Filter the posts"),
+      
+      # Input: Posted by
+      textInput(
+        inputId = "posted_by", 
+        label = "Posted by", 
+        placeholder = "Bluesky handle or display name includes ..."
+      ),
+      
+      # Tooltip for Posted by
+      bsTooltip(
+        id = "posted_by", 
+        title = "Filter by text pattern",
+        placement = "top", 
+        options = list(container = "body")
+      ),
+      
+      # Input: Date range
+      dateRangeInput(
+        "daterange", 
+        label = "...within the date range", 
+        format = "yyyy/mm/dd", 
+        separator = HTML("&ndash;"),
+        start = as.POSIXct(Sys.Date() - 365), 
+        end = as.POSIXct(Sys.Date())
+      ),
+      
+      # Input: Text search
+      textInput(
+        inputId = "text_search", 
+        label = "...including the pattern", 
+        placeholder = "Filter by text pattern"
+      ),
+      
+      # Tooltip for Text search
+      bsTooltip(
+        id = "text_search", 
+        title = "Searching <i>us</i> catches <i>us</i>, <i>bus</i>, <i>US</i>, <i>USA</i> ...",
+        placement = "top", 
+        options = list(container = "body")
+      )
+      
+      )
+      
+    ),
+    
+    # Main panel for displaying outputs
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Table", DT::dataTableOutput(outputId = "table")),
+        tabPanel(
+          "Notes",
+          br(),
+          p(
+            "The source code is available at", 
+            a("https://github.com/resulumit/likewise.", href = "https://github.com/resulumit/likewise")
+          ),
+          p(
+            "Bluesky may limit the number of likes returned. In practice, it may return fewer than the maximum at any given request."
+          )
+        )
+      )
+    )
+  )
 )
